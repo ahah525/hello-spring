d70a5440881c30af1768b1f84d8913b878401280
ahah525 <ahah525@naver.com>
2022-05-02 15:00:45 +0900

    스프링 DB 접근 기술 - 스프링 데이터 JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -2,6 +2,7 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,26 +12,26 @@ import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
+    private final MemberRepository memberRepository;
 
-    private final EntityManager em;
-
-    // SpringConfig가 EntityManger 의존(생성자 주입)
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    // 스프링 데이터 JPA가 만든 인터페이스 구현체(memberRepository) 자동 주입
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
-    // 스프링 빈에 등록
+    // memberService 스프링 빈 등록
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());   // 의존 관계 설정
+        return new MemberService(memberRepository);   // memberService가 memberRepository 의존
     }
     // 나중에 DB가 정해지면 바뀔 부분
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);    // JdbcMemberRepository가 dataSource 의존
-        //return new JdbcTemplateMemberRepository(dataSource); // JdbcTempalteRepository가 dataSource 의존
-        return new JpaMemberRepository(em); // JpaMemberRepository가 EntityManager 의존
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);    // JdbcMemberRepository가 dataSource 의존
+//        //return new JdbcTemplateMemberRepository(dataSource); // JdbcTempalteRepository가 dataSource 의존
+//        //return new JpaMemberRepository(em); // JpaMemberRepository가 EntityManager 의존
+//    }
 
 }
